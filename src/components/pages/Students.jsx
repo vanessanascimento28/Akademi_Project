bd951d6f5ff6c8b495b9a9bbd9276346266485fc
Vanessa Nascimento <vanessa.ldr.n@gmail.com>
2025-10-07 11:51:52 -0300

    "feat: add and style action buttons on Students page"

--- src/components/pages/Students.jsx
+++ src/components/pages/Students.jsx
@@ -1,7 +1,33 @@
+import FilterIcon from "../../assets/images/icons/FilterIcon.svg";
+import AddIcon from "../../assets/images/icons/AddIcon.svg";
+
 export default function Students() {
   return (
-    <section>
-      <h2 className="page__title">Students</h2>
+    <section className="page">
+      <header className="page__header">
+        <h2 className="page__title">Students</h2>
+      </header>
+      <section className="page__actions">
+        <button className="btn btn--primary">
+          Newest
+          <img
+            src={FilterIcon}
+            alt="botão de abrir menu de filtros"
+            className="btn__icon btn__icon--right"
+          />
+        </button>
+        <button className="btn btn--secondary">
+          <img
+            src={AddIcon}
+            alt="botão para adicionar um novo estudante"
+            className="btn__icon btn__icon--left"
+          />
+          New Student
+        </button>
+      </section>
+      <div className="students__table-wrap">
+        <table className="students__table" role="table"></table>
+      </div>
     </section>
   );
 }
